97ee52a42cf49d31c465a011043e51ad50aecc77
Matheus Littig <littig.works@gmail.com>
2022-03-03 23:58:36 -0400

    feat: stitches config

---
+++ src/config/global/global-styles.ts
@@ -0,0 +1,12 @@
+import { globalCss } from '..';
+
+const globalStyles = globalCss({
+  body: {
+    margin: 0,
+    padding: 0,
+    backgroundColor: '$references$background',
+    overflowY: 'scroll',
+  },
+});
+
+export { globalStyles };
