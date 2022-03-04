d5732e9fc4e0d385c6c099d486dca4f0e5f2def5
Matheus Littig <littig.works@gmail.com>
2022-03-03 23:57:47 -0400

    feat: stitches provider component

---
+++ src/components/layout/theme-provider/index.tsx
@@ -0,0 +1,8 @@
+import { globalStyles } from '@/config/global';
+import { ThemeProviderProps } from './types';
+
+export const ThemeProvider = (props: ThemeProviderProps) => {
+  globalStyles();
+
+  return props.children;
+};
